6fd4432875315d389de4673ecfaf4f33b68822b3
Alexandru Rosianu <me@aluxian.com>
2023-08-18 14:22:09 +0100

    allow moving if dst already exists

--- trash.zsh
+++ trash.zsh
@@ -1,4 +1,11 @@
 function trash() {
   mkdir -p ~/.Trash
-  command mv -v "$@" ~/.Trash
+  for file in "$@" ; do
+    dst=~/.Trash/$(basename "$file")
+    suffix=1
+    while [ -e "$dst$suffix" ]; do
+      suffix=$((suffix + 1))
+    done
+    command mv -v "$file" "$dst$suffix"
+  done
 }
